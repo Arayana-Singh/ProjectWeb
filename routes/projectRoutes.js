24512cfa97823b54c7e3c41928208ee623a133bf
ARNAV NEGI <123056923+negiarnav03@users.noreply.github.com>
2025-08-18 12:28:41 +0530

    my end first commit

---
+++ routes/projectRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const Team = require("../models/Team");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const team = new Team({ ...req.body });
+  await team.save();
+  res.json(team);
+});
+
+router.get("/", async (req, res) => {
+  const teams = await Team.find().populate("members");
+  res.json(teams);
+});
+
+module.exports = router;
---
+++ routes/teamRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Team = require("../models/Team");
+const router = express.Router();
+
+// Create Team
+router.post("/", async (req, res) => {
+  try {
+    const team = new Team(req.body);
+    await team.save();
+    res.status(201).json(team);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get All Teams
+router.get("/", async (req, res) => {
+  try {
+    const teams = await Team.find().populate("members");
+    res.json(teams);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+module.exports = router;
